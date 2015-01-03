563a2179515b631e0a032e8a03e9b84c3fbe94cd
Ali-iust <Ali-iust@users.noreply.github.com>
2015-01-03 05:16:41 +0330

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-Pattern
-=======
-public class main {
-    public static void main(String[] args) throws IOException{
-        FileReader f = new FileReader();
-        f.createDictionary();
-    }
-}
